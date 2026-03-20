7f677db20729722c13457c2f21316de13d7400e0
Jubal Martin <jubalmartin@hotmail.com>
2026-03-20 13:26:25 -0400

    Reformatted the file names so they sort nicely, also more readable

---
+++ fcc_daily_2026_03_19_invertedmatrix.py
@@ -0,0 +1,15 @@
+#Given a matrix (an array of arrays) filled with two distinct values, return a new matrix where all occurrences of one value are swapped with the other.
+def invert_matrix(matrix):
+    
+    return matrix
+
+invert_matrix([["a", "b"], ["a", "a"]]) 
+#should return [["b", "a"], ["b", "b"]].
+invert_matrix([[1, 0, 1], [1, 1, 1], [0, 1, 0]]) 
+#should return [[0, 1, 0], [0, 0, 0], [1, 0, 1]].
+invert_matrix([["apple", "banana", "banana", "apple"], ["banana", "apple", "apple", "banana"], ["banana", "banana", "banana", "apple"]]) 
+#should return [["banana", "apple", "apple", "banana"], ["apple", "banana", "banana", "apple"], ["apple", "apple", "apple", "banana"]].
+invert_matrix([[6, 7, 7, 7, 6], [7, 6, 7, 6, 7], [7, 7, 6, 7, 7], [7, 6, 7, 6, 7], [6, 7, 7, 7, 6]]) 
+#should return [[7, 6, 6, 6, 7], [6, 7, 6, 7, 6], [6, 6, 7, 6, 6], [6, 7, 6, 7, 6], [7, 6, 6, 6, 7]].
+invert_matrix([[1.2, 2.1, 2.1, 2.1], [2.1, 1.2, 2.1, 1.2], [1.2, 1.2, 2.1, 2.1]]) 
+#should return [[2.1, 1.2, 1.2, 1.2], [1.2, 2.1, 1.2, 2.1], [2.1, 2.1, 1.2, 1.2]].

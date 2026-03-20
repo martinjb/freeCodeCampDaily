00064995638adf6c885ce059605975b158c1d01a
Jubal Martin <jubalmartin@hotmail.com>
2026-03-20 15:51:10 -0400

    This one was tricky, Important detail to use enumerate in the second loop. There is probably a way to do this one in O(n).

--- fcc_daily_2026_03_19_invertedmatrix.py
+++ fcc_daily_2026_03_19_invertedmatrix.py
@@ -1,15 +1,32 @@
 #Given a matrix (an array of arrays) filled with two distinct values, return a new matrix where all occurrences of one value are swapped with the other.
 def invert_matrix(matrix):
+    status = False
+    A = None
+    B = None
+
+    for arr in matrix:
+        for element in arr:
+            if A is None:
+                A = element
+                print("A", A)
+            if B is None and element != A:
+                B = element
+                print("B", B)
+            if A and B:
+                status = True
+                break
+        if status:
+            break
     
+    for arr in matrix:
+        for i, element in enumerate(arr):
+            if element == A:
+                arr[i] = B
+            elif element == B:
+                arr[i] = A
+                
+    print(matrix)    
     return matrix
 
 invert_matrix([["a", "b"], ["a", "a"]]) 
 #should return [["b", "a"], ["b", "b"]].
-invert_matrix([[1, 0, 1], [1, 1, 1], [0, 1, 0]]) 
-#should return [[0, 1, 0], [0, 0, 0], [1, 0, 1]].
-invert_matrix([["apple", "banana", "banana", "apple"], ["banana", "apple", "apple", "banana"], ["banana", "banana", "banana", "apple"]]) 
-#should return [["banana", "apple", "apple", "banana"], ["apple", "banana", "banana", "apple"], ["apple", "apple", "apple", "banana"]].
-invert_matrix([[6, 7, 7, 7, 6], [7, 6, 7, 6, 7], [7, 7, 6, 7, 7], [7, 6, 7, 6, 7], [6, 7, 7, 7, 6]]) 
-#should return [[7, 6, 6, 6, 7], [6, 7, 6, 7, 6], [6, 6, 7, 6, 6], [6, 7, 6, 7, 6], [7, 6, 6, 6, 7]].
-invert_matrix([[1.2, 2.1, 2.1, 2.1], [2.1, 1.2, 2.1, 1.2], [1.2, 1.2, 2.1, 2.1]]) 
-#should return [[2.1, 1.2, 1.2, 1.2], [1.2, 2.1, 1.2, 2.1], [2.1, 2.1, 1.2, 1.2]].
